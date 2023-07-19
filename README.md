9459b73c27605b25bbc09d00adb2e5b22cc4ba41
BigRed85 <47097151+BigRed85@users.noreply.github.com>
2023-07-19 15:58:22 -0600

    1.0.3

--- README.md
+++ README.md
@@ -1,8 +1,211 @@
 ﻿# camera_animation
 This JS package contains classes that can be used to animate a ThreeJS camera.
+Currently it only contains one type of camera animation called a CameraAnimationCurve that uses a Catmull–Rom spline to create a path for the camera to follow. It is writen in js and does not have any type checking currently so be carful when using this.
 
-## version 1.0.1
+In the future there will be other types of animations and more type checking(I am hoping to move over to ts once I have learend more).
+
+## Version History
+
+### 1.0.3
+Added the CameraKeyframe class. Animations should now take an array of keyframes. This should simplify the interface and make the animations more predictible. 
+
+Small bug fixes.
+
+### version 1.0.2
+Added a CameraDirector class that will act as a controler for the difrent animations. The CameraDirector can pause, start and stop animations. It has a queue animaiton may be added and each animation will play one after the other (so long as the animaiton are not looping). It is able to smothly transition between animations, and controll the sepeed of the animations. 
+
+### version 1.0.1
 Added a CameraAnimation class that all camera animations will be subclasses of.
 
-### CameraAnimationCurve
-It creates a curve path for the camera to follow; it may be a looping path.
+## important Classes, Functions and Constants
+
+### Constants
+NO_TRANSITON : flag for the CameraDirector
+LINEAR_TRANSITION : flag for the CameraDirector
+SIN_TRANSITON : flag for the CameraDirector
+QUADRADIC_TRANSITION : flag for the CameraDirector
+LOG_TRANSITION : flag for the CameraDirector
+CUSTOM_TRANSITION : flag thet tells the CameraDirector that it should use a given custom function for 
+
+
+### CameraDirector class
+Acts as a contoler of the diffrent animations. The CameraDirector can pause, start and stop animations. It has a queue animaiton may be added and each animation will play one after the other (so long as the animaiton are not looping). It is able to smothly transition between animations, and controll the sepeed of the animations. 
+
+Methods:
+- constructor({transitionType = NO_TRANSITON, 
+        transitionTime = 0, 
+        animations = [{aniamtion:null, name:null}],
+        autoPlay = true,
+        transitionFunction = null})
+    - transition Type can be any of the following:
+        NO_TRANSITON
+        LINEAR_TRANSITION
+        SIN_TRANSITON
+        QUADRADIC_TRANSITION
+        LOG_TRANSITION
+        CUSTOM_TRANSITION
+    - transitionTime should be in seconds, it will determin how long it takes to ease into the animations.
+    - animations is an array containg the animation data and the names of each anmiation, this will be the queue
+    - if autoPlay is true the first time update is called the animations will start playing, if false you must first call the play method (the animation not progress)
+    - transitionFunction is the custom function that will be used if the transition type is CUSTOM_TRANSITION, this should be a function that will take in a value in the range of 0 to 1 and return a value in the range of 0 to 1
+
+- enqueue({animation = new CameraAnimation(), name=""})
+    - adds a new animation to the animation queue
+    - must be a concreate camera animation class not the virtual class.
+    - if the name is not given then a name will be generated for the animation
+
+- setTransition({transitionType = NO_TRANSITON, transitionTime = 0,customFunction = null})
+    - sets the transition type and time
+    - transition Type can be any of the following:
+        NO_TRANSITON
+        LINEAR_TRANSITION
+        SIN_TRANSITON
+        QUADRADIC_TRANSITION
+        LOG_TRANSITION
+        CUSTOM_TRANSITION
+    - transitionTime should be in seconds, it will determin how long it takes to ease into the animations.
+    - customFunction is the custom function that will be used if the transition type is CUSTOM_TRANSITION, this should be a function that will take in a value in the range of 0 to 1 and return a value in the range of 0 to 1
+
+- play()
+    - starts playing the first animation in the queue if no animation is playing
+    - if the animations are paused it will resume all animations
+
+- pause()
+    - pauses the animations currently playing, the animations may be resumed via the play() method
+    - this is the same as seting timeScale to 0.
+
+- stop()
+    - stops all animations currently playing, this means that all animations will be reset. the fist animation in the queue will start playing from the begining when the play() method is called
+
+- next(transition = true)
+    - will shift out the first animation in the queue and start playing the next animation
+    - if transition is true the animation will be eased into using the transition function
+
+- setTimeScale(timeScale = 1) 
+    - sets the time scale, alowing the animations to be slowed or sped up 
+    - note: this will also speed up the transition time
+    - timeScale at 1 is default time scale, 2 is double speed, 0.5 is half speed
+
+- update(deltaTime = 0.0) 
+    - will update the current playing animation based on the given time delta.
+    - this is affected by time scale
+    - it will auto play on the first call if the autoPlay flage is set to true
+    - it will advance to the next animation in the queue when the first animation is finished
+    - it will initiate a transition to the next animation if the transition type is set.
+
+- isPlaying()
+    - returns a boolean that indicates if there is an animation playing
+    - note: if the animations are paused it is still considerd as playing
+
+- getAnimationNames
+    - returns an array of strings, these are the names of all the animations in the queue
+
+### CameraAnimation class
+Is a base classe for camera animations. it does not have any animation data associated with it is simply the the sceleton that all camera animations should be built on this will allow the animations to be controled by the CameraDirector class
+
+Methods:
+- constructor({timeScale = 1,  autoPlay = true, animationTime = 5, loop = false})
+    - timescale : sets the initial timeScale of the animation
+    - autoPlay : flag that determins if the animation should start playing the first time the update function is called.
+    - animationTime : the time in seconds that the animation spans
+    - loop : a flag that will determin in the animation is looping 
+
+- play()
+    - starts playing the animation, alowing the update method to advance the animation
+    - sets the timeScale to a memorized value
+
+- pause()
+    - pauses the animation
+    - sets the time scale to 0
+    - this stops the update() method from advancing the animation
+
+- stop()
+    - stops the animation
+    - sets time scale to 0
+    - resets the animation to start
+
+- isPlaying()
+    - returns true if the animation is playing, otherwise returns false
+
+- isFinished() 
+    - returns true if the animation has reached the end, otherwise returns false
+    - when the animation is looping will always return false
+
+- getTimeScale()
+    - returns the current timescale
+
+- setTimeScale(timeScale) 
+    - sets the time scale to the given value
+    - if timeScale is 0 it will pause the animation
+    - if timeScale is not 0 it will call the play method
+    
+- getAutoPlay()
+    - returns the autoPlay flag
+
+- setAutoPlay(bool = !this.#autoPlay)
+    - sets the autoPlay flag to the given value
+    - if no value is given it will togal the value
+
+- update(deltaTime = 0)
+    - advances the animation based on the given time delta
+    - if autoPlay is true on the first call then the animation will start playing and the time scale will be set to the memorised value
+
+- updateByTime(time)
+    - sets the animation based on the given time in seconds
+    - is not affected by timeScale 
+    - is not affected by the play, pause, of stop methods
+    - this should not be the default update method for an animation 
+    - this should only be used if the time delta is not available
+
+- getT()
+    - returns the interal variable #t 
+    - this is the fraction of the animation compleated 
+    - #t = 0 at the start of the animation
+    - #t = 1 at the end of the animation
+
+- getAnimaitonTime()
+    - returns the length of the animation in seconds
+
+### CameraAnimationCurve class
+Extends CameraAnimation
+
+It creates a curve path, based on Catmull–Rom spline curves, for the camera to follow; it may be a looping path.
+
+Methods:
+- constructor(
+        { camera=null, 
+            keyframes=[],
+            animationTime = 5.0, 
+            loop = false, 
+            autoPlay = true, 
+            timeScale = 1.0})
+    - camera : is the threeJS camera that will be animated, this MUST be given
+    - keyframes : is an array that should be filled with CameraKeyFrame objects, each keyframe will be equaly spaced out in the timeline with the first entry being the cameras initital position and the last entry being the final position
+    - animationTime : the time in seconds it will take for the animation to be compleate
+    - loop : a flag that determins if the animation is looping and the curves are closed
+    - autoPlay : a flag the determins if the animation should play the first time the update method is called
+    - timeScale : the speed of the animation, see CameraAnimation base class for more details
+
+- update(deltaTime, weight = 1) 
+    - updates the camera position based on a time delta
+    - the weight deterimis how strong the animation should pull the camera, used for smoother transitions
+    - see CameraAnimation base class for more detail
+
+- updateByTime(time)
+    - update the camera based on the given time 
+    - see CameraAnimation base class for more detail
+
+### CameraKeyFrame class
+A class that is used by camera animations. It contains a position, a lookAt value, and an up direction.
+- constructor(
+        {position = new THREE.Vector3(0, 0, 0), 
+            lookAt = new THREE.Vector3(0, 0, 1), 
+            up = new THREE.Vector3(0, 1, 0)}) 
+
+properties:
+- position: 
+    - a Vector3 value
+- lookAt:
+    - a Vector3 value
+- up:
+    - a Vector3 value
--- camera_animation.js
+++ camera_animation.js
@@ -4,14 +4,264 @@
 
 import * as THREE from "three";
 
+export const NO_TRANSITON = 0;
+export const LINEAR_TRANSITION = 1; 
+export const SIN_TRANSITON = 2;
+export const QUADRADIC_TRANSITION = 3;
+export const LOG_TRANSITION = 4;
+export const CUSTOM_TRANSITION = 10;
+
+function linearTransition(fraction) {
+    return fraction;
+}
+
+function sinTransition(fraction) {
+    return Math.sin(fraction * Math.PI / 2);
+}
+
+function quadradicTransition(fraction) {
+    return fraction * fraction;
+}
+
+function logTransition(fraction) {
+    return (Math.log(fraction) + 4) / 4; 
+}
+
+
+//curently no transiton, will work on this in a next update.
+export class CameraDirector {
+    //this class will manage multiple camera animations including transitioning between animations, creating an animation queue, etc.
+    #transitionType; //a flag that tells the type of transition that will take place
+    #transitionTime; //the time it will take to fully transition to the next animation
+    #inTransition; // a boolean flag the indicates if a transition has started;
+    #inTransitionNext;
+    #transitionStartTime; //the time that the next transition should start
+    #transitionStartTimecurrent; //the start time of the current transition; 
+    #transitionFunction;
+    
+    #animations;
+    #elapsedTime;
+    #finishTime;
+    #isPlaying;
+    #timeScale;
+    #timeScaleMem;
+    #firstUpdate;
+    #autoPlay;
+
+
+
+    //clock should be given by user to ensure that the program is running of the same clock, if the clock is not handed over an error message will apear
+    constructor({transitionType = NO_TRANSITON, 
+        transitionTime = 0, 
+        animations = [{aniamtion:null, name:null}],
+        autoPlay = true,
+        transitionFunction = null}) {
+        
+        this.setTransition({transitionType:transitionType, transitionTime:transitionTime, customFunction:transitionFunction});
+        this.#inTransition = false;
+        this.#inTransitionNext = false;
+        
+        
+        this.#animations = animations;
+        this.#isPlaying = false;
+        this.#elapsedTime = 0;
+        this.#timeScale = 1;
+        this.#timeScaleMem = 1;
+        this.#firstUpdate = true;
+        this.#autoPlay = autoPlay;
+
+
+    }
+
+    enqueue({animation = new CameraAnimation(), name=""}) { //queues a new animation
+        this.#animations.push({animation:animation, name:name});
+    }
+
+    setTransition({transitionType = NO_TRANSITON, transitionTime = 0, customFunction = null}) {
+        
+        this.#transitionType = transitionType;
+        this.#transitionTime = transitionTime;
+
+        switch (transitionType) {
+            
+            case LINEAR_TRANSITION:
+                this.#transitionFunction = linearTransition;
+                break;
+            case QUADRADIC_TRANSITION:
+                this.#transitionFunction = quadradicTransition;
+                break;
+            case SIN_TRANSITON:
+                this.#transitionFunction = sinTransition;
+                break;
+            case LOG_TRANSITION:
+                this.#transitionFunction = logTransition;
+                break;
+            case CUSTOM_TRANSITION:
+                //check to make sure there is a function in the cutom function space.
+                break;
+            case NO_TRANSITON:
+            default:
+                this.#transitionFunction = null;
+                break;
+        }
+    }
+
+    play() {
+        this.#play();
+    }
+
+    #play(index = 0) { //start playing the animation at the front of the queue
+        //will need the start time of each animation, and the total time for the animation 
+        if (this.#animations.length <= index)
+            return false;
+
+        if (!this.#isPlaying) { //if not paused or currently playing
+            let animationTime = this.#animations[index].animation.getAnimaitonTime();
+            this.#finishTime = this.#elapsedTime + animationTime;
+            this.#transitionStartTime = this.#finishTime - this.#transitionTime;
+            this.#isPlaying = true;
+        }
+        
+        this.#animations[index].animation.play();
+        this.#timeScale = this.#timeScaleMem;
+
+        return true;
+    }
+
+    pause() {
+        this.#timeScale = 0;
+    }
+
+    stop() {
+        this.#isPlaying = false;
+        this.#animations[0].animation.stop();
+        //will also have to stop the next animation and the transition
+        if(this.#inTransition) {
+            this.#animations[1].animation.stop();
+            this.#inTransition = false;
+        }
+    }
+
+    next(transition = true) { //will start playing the next animation in the queue (by default with transition). returns true if the next animaiton has started playing
+        if (this.#animations.length < 1)
+            return false;
+
+        this.#animations.shift();
+        if (transition) {
+            this.#inTransitionNext = true;
+            this.#transitionStartTimecurrent = this.#elapsedTime;
+        }
+        
+        return this.#play();
+    }
+
+    setTimeScale(timeScale = 1) {
+        this.#timeScaleMem = timeScale;
+        
+        this.#play();
+    }
+
+    update(deltaTime = 0.0) {
+        //will update the current playing animation, if we are transitioning to a next animation we should hadle that when an animation is finished we should start the next, if all animations are finished do nothing.
+        
+        if (this.#animations.length === 0)
+            return;
+
+        if (this.#firstUpdate && this.#autoPlay) {
+            this.#firstUpdate = false;
+            this.#play(0);
+        }
+        else if (this.#firstUpdate) {
+            this.#firstUpdate = false;
+        }
+        
+        if (this.#animations[0].animation.isFinished()) {
+            if (!this.next(false)) {
+                return;
+            }
+            this.#inTransition = false;
+        }
+        
+        deltaTime = deltaTime * this.#timeScale;
+
+        this.#elapsedTime += deltaTime;
+
+        if (this.#inTransitionNext) {
+            let weight = this.#transitionCalc();
+            this.#animations[0].animation.update(deltaTime, weight);
+        }
+        else {
+            this.#animations[0].animation.update(deltaTime);
+        }
+        
+        if (this.#inTransition) {
+            //there are two types of transition either form
+            let weight = this.#transitionCalc(); 
+            this.#animations[1].animation.update(deltaTime, weight);
+        } 
+
+        if (this.#transitionType && !this.#inTransition && this.#elapsedTime >= this.#transitionStartTime && this.#animations.length > 1) {
+            //start transitioning to the next animation
+            let transitionDelta = this.#elapsedTime - this.#transitionStartTime;
+            this.#transitionStartTimecurrent = this.#transitionStartTime;
+            this.#play(1);
+            this.#inTransition = true;
+            this.#animations[1].animation.update(transitionDelta);
+        }
+
+    }
+
+    //returns the wight [0,1] calculated using the transition function
+    #transitionCalc() {
+        
+        if (!this.#transitionFunction)
+            return 1;
+
+        let fraction = (this.#elapsedTime - this.#transitionStartTimecurrent) / this.#transitionTime;
+        return this.#transitionFunction(fraction);
+    }
+
+    isPlaying() {
+        return this.#isPlaying;
+    }
+
+    //returns an array containing the names of all the animation in the queue
+    getAnimationNames() {
+        let names = []
+        for (let i = 0; i < this.#animations.length; i++) {
+            names.push(this.#animations[i].name)
+        }
+
+        return names;
+    }
+    
+    #debugDump(deltaTime) {
+        console.log(
+            "#transitionType" + this.#transitionType + 
+            "#transitionTime" + this.#transitionTime + 
+            "#inTransition;" + this.#inTransition + 
+            "#transitionStartTime;"  + this.#transitionStartTime +
+            "#animations;" + this.#animations + 
+            "#elapsedTime;" + this.#elapsedTime + 
+            "#nextTime;" + this.#finishTime + 
+            "#isPlaying;" + this.#isPlaying + 
+            "#timeScale;" + this.#timeScale + 
+            "#timeScaleMem;" + this.#timeScaleMem + 
+            "deltaTime: " + deltaTime
+        );
+    }
+}
+
 class CameraAnimation { //this is a base class for all camera animations; it should not be instantiated.
     #timeScale;
     #timeScaleMem;
     #autoPlay;
+    #animationTime;
     #first_flag;
     #play_flag;
     #t;
-    #loop
+    #loop;
+    #isFinished;
 
 
     //this is a abstract class that defig some basic operation that all camera animation will use
@@ -20,33 +270,39 @@ class CameraAnimation { //this is a base class for all camera animations; it sho
         this.#timeScale = timeScale; //this will determin the speed of the animation when updateing, alowing the animation to be speed up or slowed 
         this.#timeScaleMem = timeScale; //this will remember the last set timescale, allowing for pausing and resuming with the pervious timeScale;
         this.#autoPlay = autoPlay; //this will determin if the animation will start playing on the first update call
-        this.animationTime = animationTime; //the duration of the animation in seconds
+        this.#animationTime = animationTime; //the duration of the animation in seconds
         this.#loop = loop //boolean flag that indicates if the animation loops
 
         this.#first_flag = true;
         this.#play_flag = false; //flag indicating that the animation is playing
         this.#t = 0.0; // keep track of the animation state. Is a float [0,1]; 0 = start of animation, 1 = end of animation.
+        this.#isFinished = false;
 
 
     }
 
     play() {
         this.#play_flag = true;
-        this.timeScale = this.#timeScaleMem; //play at the memorized timeScale;
+        this.#timeScale = this.#timeScaleMem; //play at the memorized timeScale;
     }
 
     pause() { //pauses the animation 
         this.#play_flag = false;
-        this.#timeScale = 0.0;
+        this.#timeScale = 0;
     }
 
     stop() {
         this.#play_flag = false;
         this.#t = 0;
+        this.#timeScale = 0;
     }
 
     isPlaying() {
         return this.#play_flag;
+    }
+
+    isFinished() {
+        return this.#isFinished;
     }
 
     getTimeScale() {
@@ -66,35 +322,47 @@ class CameraAnimation { //this is a base class for all camera animations; it sho
         return this.#autoPlay;
     }
 
-    setAutoPlay({bool=!this.#autoPlay}) {
+    setAutoPlay(bool=!this.#autoPlay) {
         this.#autoPlay = bool;
     }
 
-    update({delta = 0, animationTime = 5}) { //will update the animation by a delta time 
+    update(deltaTime = 0) { //will update the animation by a delta time 
         if (this.#first_flag) {
             this.#first_flag = false;
             if (this.#autoPlay)
-                this.#play_flag = true;
-                this.#timeScale = 1.0;
+                this.play()
         } 
 
 
-        console.assert(typeof delta === "number", "CameraAnimationCurve::update Error: delta must be a number");
-        this.#t += (delta / animationTime) * this.#timeScale;
-        //this.#clamp_t();
+        console.assert(typeof deltaTime === "number", "CameraAnimationCurve::update Error: delta must be a number");
+        this.#t += (deltaTime / this.#animationTime) * this.#timeScale;
+        this.#clamp_t();
+
+        //this.#debugDump(deltaTime);
     }
 
     updateByTime(time) { //will update an animation based on a given time (note: this will not be affected by the timeScale variable, or the play state)
-        this.#t = time / this.animationTime;
-        //this.#clamp_t();
+        this.#t = time / this.#animationTime;
+        this.#clamp_t();
     }
 
     getT() {
         return this.#t;
     }
 
+    getAnimaitonTime() {
+        return this.#animationTime;
+    }
+
     #clamp_t() {
-        this.#t = (this.#loop === false && this.#t > 1.0) ? 1.0 : this.#t;
+        if (this.#loop)
+            return;
+        
+        this.#t = (this.#t > 1.0) ? 1.0 : this.#t;
+
+        if (this.#t === 1) {
+            this.#isFinished = true;
+        }
     }
 
     #debugDump(delta) {
@@ -120,29 +388,30 @@ export class CameraAnimationCurve extends CameraAnimation {
     //  idealy these arrays should be the same length, but they need not be, the first entry in the array will be the initial position and the final entry will be the final position
     //animationTime : the number of seconds that it will take for the animation to complete
     //loop : a boolean that will indicat if the animation should loop (the curves will be closed);
-    constructor({ camera=null, cameraPath=[], cameraLook=[], cameraUp=null, animationTime = 5.0, loop=false, autoPlay=true, timeScale}) {
+    constructor(
+        { camera=null, 
+            keyframes=[],
+            animationTime = 5.0, 
+            loop = false, 
+            autoPlay = true, 
+            timeScale = 1.0}) {
         
-        super({timeScale:1.0, autoPlay:autoPlay, animationTime:animationTime, loop:loop})
+        super({timeScale:timeScale, autoPlay:autoPlay, animationTime:animationTime, loop:loop})
         console.assert(camera !== null, "CameraAnimationCurve Error: camera must not be null!")
-        console.assert(cameraPath.length >= 2, "CameraAnimationCurve Error: cameraPath array must have at least 2 positions");
-        console.assert(cameraLook.length >= 2, "CameraAnimationCurve Error: cameraLook array must have at least 2 positions");
-        //should move over to type script and check that the arrays is full of vector3
+        console.assert(keyframes.length >= 2, "CameraAnimationCurve Error: keyframes array must have at least 2 positions")
         
         this.camera = camera;
-        this.cameraPath = cameraPath;
-        this.cameraLook = cameraLook;
-        this.loop = loop;
+        this.keyframes = keyframes;
+        this.cameraPath = [];
+        this.cameraLook = [];
+        this.cameraUp = [];
 
-        if (cameraUp !== null) {
-            if (Array.isArray(cameraUp))
-                this.cameraUp = cameraUp;
-            else 
-                this.cameraUp = [cameraUp, cameraUp];
-        }
-        else {
-            this.cameraUp = [THREE.Object3D.DEFAULT_UP, THREE.Object3D.DEFAULT_UP];
-        } 
-        
+        this.keyframes.forEach(keyframe => {
+            this.cameraPath.push(keyframe.position);
+            this.cameraLook.push(keyframe.lookAt);
+            this.cameraUp.push(keyframe.up);
+        });
+
         this.#calculateCurves();
     }
 
@@ -153,21 +422,25 @@ export class CameraAnimationCurve extends CameraAnimation {
     }
 
     //updates the animation based a given delta time
-    update(delta) {
-        super.update({delta:delta, animationTime:this.animationTime});
-        this.#setCamera()
+    update(deltaTime, weight = 1) {
+        super.update(deltaTime);
+        this.#setCamera(weight);
     }
 
     updateByTime(time) {
-        super.updateByTime(time, this.animationTime);
+        super.updateByTime(time);
         this.#setCamera();
     }
 
 
-    #setCamera() {
-        this.camera.position.copy( this.pathCurve.getPoint(this.getT()) );
-        this.camera.up.copy( this.upCurve.getPoint(this.getT()) );
+    #setCamera(weight = 1) {
+        this.camera.position.lerp( this.pathCurve.getPoint(this.getT()), weight );
+        this.camera.up.lerp( this.upCurve.getPoint(this.getT()), weight );
+        
+        let camRotaion = this.camera.quaternion;
         this.camera.lookAt( this.lookCurve.getPoint(this.getT()) );
+        camRotaion.slerp(this.camera.quaternion, weight);
+        this.camera.setRotationFromQuaternion(camRotaion);
     }
 
     #debugDump() {
@@ -176,6 +449,18 @@ export class CameraAnimationCurve extends CameraAnimation {
             "lookCurve point: " + this.lookCurve.getPoint(this.getT()) + 
             "camera: " + JSON.stringify(this.camera) + 
             "this: " + this
-        )
+        );
+    }
+}
+
+export class CameraKeyFrame {
+    constructor(
+        {position = new THREE.Vector3(0, 0, 0), 
+            lookAt = new THREE.Vector3(0, 0, 1), 
+            up = new THREE.Vector3(0, 1, 0)}) 
+    {
+        this.position = position;
+        this.lookAt = lookAt;
+        this.up = up;    
     }
 }
